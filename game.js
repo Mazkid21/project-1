1c1504021e7859d71158d478ef71981c9c546249
Mazkid21 <zmaraziti@yahoo.com>
2017-07-14 15:28:13 -0600

    snake stays in board, food appearing

--- game.js
+++ game.js
@@ -14,7 +14,7 @@ $(document).keyup(function(e) {
 
 $("#startGame").click(function () {
     var counter = 30;
-    setInterval(function() {
+    var interval = setInterval(function() {
         counter--;
             if (counter >= 0) {
                 span = $("#count");
@@ -22,18 +22,20 @@ $("#startGame").click(function () {
                 $("#startGame").attr("disabled", "disabled");
             }
             if (counter === 0) {
-                clearInterval(counter);
+                clearInterval(interval);
                 return  $("#game").toggleClass("gameBoard"), 
                         $("#snake").toggleClass("player"), 
                         $("#win").css('display', 'block'), 
                         $("#startGame").css('display', 'none'), 
                         $("#reload").css('display', 'block'),
-                        $("startGame").removeAttr("disabled", "disabled");
+                        $("startGame").removeAttr("disabled", "disabled"),
+                        $(".food").toggleClass("food");
             }
     }, 1000);
 
     $("#game").toggleClass("gameBoard");
     $("#snake").toggleClass("player");
+    makeDiv();
 
 });
 
@@ -47,6 +49,7 @@ $("#reload").click(function () {
                 $("#win").css('display', 'none');
                 $("#startGame").css('display', 'block');
                 $("#startGame").attr("disabled", "disabled");
+
             }
             if (counter === 0) {
                 clearInterval(counter);
@@ -55,17 +58,21 @@ $("#reload").click(function () {
                         $("#win").css('display', 'block'), 
                         $("#startGame").css('display', 'none'),
                         $("#reload").css('display', 'block'),
-                        $("#reload").removeAttr("disabled", "disabled");
+                        $("#reload").removeAttr("disabled", "disabled"),
+                        $(".food").toggleClass("food");
                         
             }
     }, 1000);
 
     $("#game").toggleClass("gameBoard");
     $("#snake").toggleClass("player");
+    $(".player").attr('style','');
+    makeDiv();
 
 });
 
 function movePlayer() {
+    
     var position = $(".player").position();
 
     checkCollisions();
@@ -73,20 +80,24 @@ function movePlayer() {
     for (var direction in keys) {
         if (!keys.hasOwnProperty(direction)) continue;
         if (direction == 37) {
+            if ($(".player").css('left') != '9px') {
             $(".player").css('left', position.left - 3 + 'px');
-        } 
+        } }
 
         if (direction == 38) {
+            if ($(".player").css('top') != '98px') {
             $(".player").css('top', position.top - 3 + 'px');
-        }
+        } }
 
         if (direction == 39) {
+            if ($(".player").css('left') != '696px') {
             $(".player").css('left', position.left + 3 + 'px');
-        }
+        }}
 
-        if (direction == 40) {
+        if (direction == 40) { 
+            if  ($(".player").css('top') != '578px') {
             $(".player").css('top', position.top + 3 + 'px');
-        }
+        }}
 
         
 
@@ -115,31 +126,38 @@ function getPositions(element) {
     // return [[ testX, testX + testWidth], [testY, testY + testHeight]];
 
 
-     var left1 = element.css('left'); // THIS IS THE X 
-     var left  = element.css('left');
-     var top1 = element.css('top');
-     var top = element.css('top');
-     var width = element.innerWidth();
-     var height = element.innerHeight();
+     var left1 = parseInt(element.css('left'), 10); // THIS IS THE 
+        // console.log(left1); 
+     var left  = parseInt(element.css('left'), 10);
+        // console.log(left); 
+     var top1 = parseInt(element.css('top'), 10);
+        // console.log(top1); 
+     var top = parseInt(element.css('top'), 10);
+        // console.log(top); 
+     var width = parseInt(element.css('width'), 10);
+           // console.log(width); 
+     var height = parseInt(element.css('height'), 10);
       return [[ left1, left + width ], [ top1, top + height]];
 
 }
 
- function comparePositions(p1, p2){
+ function comparePositions(smallerThing, biggerThing){
     // p1[0][0] is p1's left boundary
     // p1[1][0] is p1's top boundary
     // p1[0][1] is p1's right boundary
     // p1[1][1] is p1's bottom boundary
 
-    var p1Left = p1[0][0];
-    var p1Top = p1[1][0];
-    var p1Right = p1[0][1];
-    var p1Bottom = p1[1][1];
+    var p1Left = smallerThing[0][0];
+    //console.log(p1Left);
+    var p1Top = smallerThing[1][0];
+    var p1Right = smallerThing[0][1];
+    var p1Bottom = smallerThing[1][1];
 
-    var p2Left = p2[0][0];
-    var p2Top = p2[1][0];
-    var p2Right = p2[0][1];
-    var p2Bottom = p2[1][1];
+    var p2Left = biggerThing[0][0];
+    //console.log(p2Left);
+    var p2Top = biggerThing[1][0];
+    var p2Right = biggerThing[0][1];
+    var p2Bottom = biggerThing[1][1];
 
     // if p1's left boundary is greater than p2's left boundary
     // AND p1's right boundary is less than p2's right boundary
@@ -150,12 +168,13 @@ function getPositions(element) {
 
     // console.log(p1Left);
 
-    if (p1Left >= p2Left && p1Right <= p2Right && p1Top <= p2Top && p1Bottom >= p2Bottom) {
-        return console.log('true');
+    //if(p1Left >= p2Left) console.log('DFHSD');
+    if (p1Left >= p2Left && p1Right <= p2Right && p1Top >= p2Top && p1Bottom <= p2Bottom) {
+        return  true;  // console.log('true');
     }
 
     else { 
-        return console.log('false') ;
+        return false;  //console.log('false') ;
     }
 
     // IF NOT all that shit.
@@ -170,19 +189,41 @@ function getPositions(element) {
 
 function checkCollisions(){
     
-    var box = $("#game");
+    var player = $("#snake");
+       // console.log(box);
 
-     var pos = getPositions(box);
+     var pos = getPositions(player);
+        
 
-     var player = $("#snake");
-     var pos2 = getPositions(player);
+     var food = $(".food");
+     //  console.log(player);
+     var pos2 = getPositions(food);
+        console.log("food: " + pos2);
 
-     var match = comparePositions(pos, pos2);
+     var match = comparePositions(pos2, pos);
+      //console.log(match);
 
-     if (match) { console.log("yayayayyayaya"); 
+     if (match) {console.log("yayay"); }
 
-     console.log(pos);
+   //  console.log(pos);
+ 
  }
- }
 
+ 
+ function makeDiv() {
+        var numRand = Math.floor(Math.random() * 501);
+        
+        var divsize = 10;
+    
+        var posx = (Math.random() * ($(".gameBoard").width() - divsize)).toFixed();
+        var posy = (Math.random() * ($(".gameBoard").height() - divsize)).toFixed();
+        $newdiv = $("<div class='food'></div>").css({
+            'left': posx + 'px',
+                'top': posy + 'px'
+        });
+        $newdiv.appendTo('.gameBoard').delay(1000).fadeIn(100, function () {
+            //$(this).remove();
+            makeDiv();
+        });
+    }
 
