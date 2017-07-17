200d2b151527895ac635f7a71627515ede47b5a0
Mazkid21 <zmaraziti@yahoo.com>
2017-07-17 10:15:13 -0600

    cleaning up code and commenting

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
+// setting interval to 30 milseconds 
 setInterval(movePlayer, 30);
 var keys = {};
 
-// Function to make the q and p key move racers 
+// Function to make key fucntions work 
 $(document).keydown(function(e) {
     keys[e.keyCode] = true;
 });
@@ -11,7 +12,7 @@ $(document).keyup(function(e) {
 });
 
 
-
+// Start game button. Event listener function. Starts 30 second counter, adds game board and snake and food to screen 
 $("#startGame").click(function () {
     var counter = 30;
     var interval = setInterval(function() {
@@ -21,6 +22,7 @@ $("#startGame").click(function () {
                 span.html(counter);
                 $("#startGame").attr("disabled", "disabled");
             }
+            // when counter gets to zero, take game board food counter and snake away. Brings up player 1s score and a player 2 button. 
             if (counter === 0) {
                 clearInterval(interval);
                 return  $("#game").toggleClass("gameBoard"), 
@@ -35,6 +37,7 @@ $("#startGame").click(function () {
             }
     }, 1000);
 
+    // adds game board food and snake to board, calls make food function. 
     $("#game").toggleClass("gameBoard");
     $("#snake").toggleClass("player");
     $("#white").toggleClass("food");
@@ -43,6 +46,8 @@ $("#startGame").click(function () {
     
 
 });
+
+// player 2 button, does the same as start game but brings up player 2 score board and win state 
 
 $("#reload").click(function () {
     var counter = 30;
@@ -81,6 +86,8 @@ $("#reload").click(function () {
 
 });
 
+// move player function that makes arrow keys move player. Also calls the checkCollisons fucntion 
+
 function movePlayer() {
     
     var position = $(".player").position();
@@ -110,59 +117,17 @@ if (keys[39]) {
             $(".player").css('top', position.top + 3 + 'px');
         }}
 
-
-
-
-    // for (var direction in keys) {
-    //     if (!keys.hasOwnProperty(direction)) continue;
-    //     if (direction == 37) {
-    //         if ($(".player").css('left') != '9px') {
-    //         $(".player").css('left', position.left - 3 + 'px');
-    //     } }
-
-    //     if (direction == 38) {
-    //         if ($(".player").css('top') != '98px') {
-    //         $(".player").css('top', position.top - 3 + 'px');
-    //     } }
-
-    //     if (direction == 39) {
-    //         if ($(".player").css('left') != '696px') {
-    //         $(".player").css('left', position.left + 3 + 'px');
-    //     }}
-
-    //     if (direction == 40) { 
-    //         if  ($(".player").css('top') != '578px') {
-    //         $(".player").css('top', position.top + 3 + 'px');
-    //     }}
-
-        
-
-
-        // if ($(".player").position.left < 3 ) {
-        // return console.log("stoppppp"); }
-
-
-    
         
 
 
 }
 
 
+// Checks the postitions of food and snake
 function getPositions(element) {
-    // element is some sort of html tag. <p> or <div> or whatever
-    // we need to get it's left, its top, its width, and its height
     
-    // var test = $("#snake").getBoundingClientRect();
-    // var testX = test.x ;
-    // var testY = test.y;
-    // var testHeight = test.height;
-    // var testWidth = test.width;
 
-    // return [[ testX, testX + testWidth], [testY, testY + testHeight]];
-
-
-     var left1 = parseInt(element.css('left'), 10); // THIS IS THE 
+     var left1 = parseInt(element.css('left'), 10); 
         // console.log(left1); 
      var left  = parseInt(element.css('left'), 10);
         // console.log(left); 
@@ -177,11 +142,9 @@ function getPositions(element) {
 
 }
 
+// compares the positions of snake and food
+
  function comparePositions(smallerThing, biggerThing){
-    // p1[0][0] is p1's left boundary
-    // p1[1][0] is p1's top boundary
-    // p1[0][1] is p1's right boundary
-    // p1[1][1] is p1's bottom boundary
 
     var p1Left = smallerThing[0][0];
     //console.log(p1Left);
@@ -200,20 +163,11 @@ function getPositions(element) {
     var p2Bottom = biggerThing[1][1];
     //console.log(p2Bottom);
 
-    // if p1's left boundary is greater than p2's left boundary
-    // AND p1's right boundary is less than p2's right boundary
-    // AND p1's top boundary is less than p2's top boundary
-    // AND p1's bottom boundary is more than p2's bottom boundary
-    // THEEEEEEEN p1 is inside p2
-    // return true;
-
-    // console.log(p1Left);
-
-    //if(p1Left >= p2Left) console.log('DFHSD');
         
-            //WORKS BUT BUGGGGGGGY 
+        //WORKS BUT BUGGGGGGGY 
        // (p1Left <= p2Left && p1Right <= p2Right && p1Top + 40  != p2Top && p1Bottom + 40 != p2Bottom  )
 
+       // if snake is over food return true
     if  (p1Left + 5 >= p2Left && p1Right + 2 <= p2Right && p1Top + 87  != p2Top && p1Bottom + 97 != p2Bottom  )
 
         // DOESNT WORK BUT SHOULD!! NEED TO PLAY WITH THE NUMBERSSSSSS
@@ -228,15 +182,9 @@ function getPositions(element) {
         return false;  //console.log('false') ;
     }
 
-    // IF NOT all that shit.
-    // return false;
-    /*
-     var x1 = p1[0] > p2[0] ? p1 : p2;
-     var x2 = p1[0] < p2[0] ? p2 : p1;
-     return x1[1] > x2[0] || x1[0] === x2[0] ? true : false;
-     */
-
  }
+
+ // Checks for collisions and if detected moves food to random position updates score. 
 
 function checkCollisions(){
     
@@ -260,40 +208,9 @@ function checkCollisions(){
 
    //  console.log(pos);
 
-        //NOT WORKING BUT WILL WORK FOR ALL FOOD
-        //THIS IS WHAT YOU NEED TO FIX TO MAKE THE CHECK COLLISONS WORK!!!!!!!!
-          //var food = $(".food");
-             //$(".food").each(function(e) { 
-             //for (i=0; i < food.length; i++) {
-             //var pos2 = getPositions();
-             //var match = comparePositions(pos2, pos);
-             //if (match) {console.log("yayay"); }
-
- 
- 
-
-
 }
 
-  
-  // function makeDiv() {
-  //        var numRand = Math.floor(Math.random() * 501);
-        
-  //        var divsize = 10;
-    
-  //        var posx = (Math.random() * ($(".gameBoard").width() - divsize)).toFixed();
-  //        var posy = (Math.random() * ($(".gameBoard").height() - divsize)).toFixed();
-  //        $newdiv = $("<div class='food'></div>").css({
-  //            'left': posx + 'px',
-  //                'top': posy + 'px'
-  //        });
-  //        $newdiv.appendTo('.gameBoard').delay(1000).fadeIn(100, function () {
-  //            //$(this).remove();
-  //            makeDiv();
-  //        });
-  //    }
-
- 
+ // Makes food and puts in random location on the gameBoard 
 
  function makeFood() {
          
@@ -316,31 +233,32 @@ function checkCollisions(){
          $newdiv.appendTo('.gameBoard');
      }
 
+// player 1's score 
+
 var score = 0 ;
 
 function updateScore() {
     
         score  += 1;
 
-    
-
-
 }
+
+// Player 2's score 
 
 var score2 = 0 ;
 
 function updateScore2() {
     
         score2  += 1;
-
-    
-
-
 }
+
+// Resets player 2's score that was being tallied 
 
 function resetScore2() {
     score2 = 0;
 }
+
+// Replay button, to refresh page to play again 
 
 $("#refresh").click(function () {
     location.reload();
